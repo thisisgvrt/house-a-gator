a2a562fd81ec922d02eb62a6ff11524942c5247f
Raviteja Guttula <thisisgvrt@gmail.com>
2020-06-16 14:53:27 -0700

    Added page for ravi

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Aboutus from "./pages/Aboutus";
 import swetha from "./pages/profile/swetha";
 import kevin from "./pages/profile/kevin";
+import ravi from "./pages/profile/ravi";
 
 function App() {
   return (
@@ -12,17 +13,17 @@ function App() {
         <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-gray border rounded-lg">
           <h3 class="my-0 mr-md-auto font-weight-bolder">Team 02</h3>
           <nav class="my-2 my-md-0 mr-md-3">
-            <Link to="/Aboutus">
+            <Link to="/About-us">
               <button class="p-2 btn btn-link" href="#" style={{ "font-size": "1.45rem" }}>About us</button>
             </Link>
           </nav>
         </div>
         <Switch>
           /// if new js page added .. you need to add in route in this file(App.js) like below
-          <Route path="/Aboutus" component={Aboutus} />
+          <Route path="/About-us" component={Aboutus} />
           <Route path="/swetha" component={swetha} />
           <Route path="/kevin" component={kevin} />
-
+          <Route path="/ravi" component={ravi} />
         </Switch>
       </Router>
     </div>
