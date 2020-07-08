7131ed33a3a7d028e3ec293436d50ca221b270e5
troyturner <troyturner444@gmail.com>
2020-07-08 16:38:15 -0700

    added login.css and updated app.js

--- application/frontend/src/pages/login.js
+++ application/frontend/src/pages/login.js
@@ -1,26 +1,29 @@
 
 import React from 'react';
+import "../css/login.css"
 import 'bootstrap/dist/css/bootstrap.css';
 
 function login() {
     return (
-        <div class ="container" style={{"padding":"3%"}}>
-            <div class="jumbotron bg-warning rounded-lg">
+        <div className ="container-fluid" style={{"padding":"3%"}}>
+            <div className="jumbotron bg-warning rounded-lg justify-content-beginning">
               <h1>Log in</h1>
+              <p>___________________</p>
             </div>
             <form>
-                <div class="form-group">
+                <div className="form-group">
                   <label for="inputEmail">Email address</label>
-                  <input type="email" class="form-control" id="inputEmail" placeholder="name@example.com"></input>
+                  <input type="email" className="form-control" id="inputEmail" placeholder="name@example.com"></input>
                 </div>
-                <div class="form-group">
-                  <lable for="inputPassword">Password</lable>g
-                  <input type="password" class="form-control" id="inputPassword" aria-describedby="passwordHelp"></input>
-                     <small id="passwordHelp" class="justify-content-end">
+                <div className="form-group">
+                  <lable for="inputPassword">Password</lable>
+                  <input type="password" placeholder="input password" className="form-control" id="inputPassword" aria-describedby="passwordHelp"></input>
+                     <small id="passwordHelp" className="justify-content-end">
                         <a class="nav-link" href="#">Forgot password?</a>
                       </small>
+                      <button type="submit" class="btn btn-info">Sign In</button>
                 </div>
-                <button type="submit" class="btn btn-info">Sign In</button>
+                
             </form>
         </div>
     );
