d0bab6cc29913d078610c8361ed63fa8def0f218
troyturner <troyturner444@gmail.com>
2020-07-09 16:17:41 -0700

    updated log in

--- application/frontend/src/pages/login.js
+++ application/frontend/src/pages/login.js
@@ -5,26 +5,30 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function login() {
     return (
-        <div className ="container-fluid" style={{"padding":"3%"}}>
-            <div className="jumbotron bg-warning rounded-lg justify-content-beginning">
+        <div class ="container-fluid" style={{"padding":"3%"}}>
+            <div class="jumbotron ">
               <h1>Log in</h1>
               <p>___________________</p>
             </div>
             <form>
-                <div className="form-group">
-                  <label for="inputEmail">Email address</label>
-                  <input type="email" className="form-control" id="inputEmail" placeholder="name@example.com"></input>
+                <div class="form-group">
+                  <label for="inputEmail">Email address:</label>
+                  <input type="email" class="form-control" id="inputEmail" placeholder="name@example.com"></input>
                 </div>
-                <div className="form-group">
-                  <lable for="inputPassword">Password</lable>
-                  <input type="password" placeholder="input password" className="form-control" id="inputPassword" aria-describedby="passwordHelp"></input>
-                     <small id="passwordHelp" className="justify-content-end">
+                <div class="form-group">
+                  <label for="inputPassword">Password:</label>
+                  <input type="password" placeholder="input password" class="form-control" id="inputPassword" aria-describedby="passwordHelp"></input>
+                     <small id="passwordHelp" class="justify-content-end">
                         <a class="nav-link" href="#">Forgot password?</a>
                       </small>
-                      <button type="submit" class="btn btn-info">Sign In</button>
+                      
                 </div>
-                
-            </form>
+                <ul class="list-inline">
+                <li class="list-inline-item"><button type="submit" class="btn btn-info">Sign In</button></li>
+                <li class="list-inline-item"><p>New User?</p></li>
+                <li class="list-inline-item"><a class="nav-link" href="#">Signup here</a></li>
+                </ul>
+                </form>
         </div>
     );
 }
