ff10ed2cc1e4cec2ed3ad97703ef8b7c0e947b19
Raviteja Guttula <thisisgvrt@gmail.com>
2020-07-11 23:19:11 -0700

    MVP for search

--- application/Backend/app/api/listing/__init__.py
+++ application/Backend/app/api/listing/__init__.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, request
 from flask_marshmallow import Marshmallow
 
-from app.database import DataTable
-
 from app.api.listing.model import Listing
 from app.api.media.model import Media
 
@@ -19,20 +17,18 @@ class ListingSchema(ma.SQLAlchemySchema):
     media = ma.List(ma.Nested(MediaSchema))
     class Meta:
         model = Listing
-        fields = ('id', 'title', 'city', 'media')
+        fields = ('title', 'city', 'media')
 
 listing_schema = ListingSchema()
 listings_schema = ListingSchema(many=True)
 
+@listing_page.route('/', methods=['POST'])
+def save_new_listing_route():
+    pass
+
+
 @listing_page.route('/')
-def index():
-    datatable = DataTable(
-        model=Listing,
-        columns=[Listing.title],
-        sortable=[Listing.title],
-        searchable=[Listing.title],
-        filterable=[],
-        limits=[25, 50, 100],
-        request=request
-    )
-    return listings_schema.jsonify(datatable.query.items)
+def get_listings_route():
+    query_string = request.args.get('query','')
+    filtered_result_set = Listing.query.filter(Listing.title.ilike(f'%{query_string}%')).all()
+    return listings_schema.jsonify(filtered_result_set)
--- application/Backend/app/api/media/__init__.py
+++ application/Backend/app/api/media/__init__.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request
-from app.database import DataTable
 
 from app.api.media.model import Media
 
@@ -7,15 +6,4 @@ media_page = Blueprint('media_page', __name__)
 
 @media_page.route('/')
 def index():
-    datatable = DataTable(
-        model=Media,
-        columns=[Media.media_path],
-        sortable=[Media.media_path],
-        searchable=[Media.media_path],
-        filterable=[],
-        limits=[25, 50, 100],
-        request=request
-    )
-    for i in datatable.query.items:
-        print(i)
     return "Welcome to Media page"
