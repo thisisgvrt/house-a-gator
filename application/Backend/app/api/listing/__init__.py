3747180d0bdf05092c65e7d55a6be4f1f9223852
Raviteja Guttula <thisisgvrt@gmail.com>
2020-07-12 20:45:55 -0700

    Added more attributes for listings API

--- application/Backend/app/api/listing/__init__.py
+++ application/Backend/app/api/listing/__init__.py
@@ -3,7 +3,7 @@ from flask_marshmallow import Marshmallow
 
 from sqlalchemy import or_
 
-from app.api.listing.model import Listing, Media
+from app.api.listing.model import Listing, Media, ListingStatus, ListingType
 
 listing_page = Blueprint('listing_page', __name__)
 
@@ -12,13 +12,25 @@ ma = Marshmallow()
 class MediaSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Media
-        fields = ('id', 'media_path')
+        fields = ('id', 'media_title', 'media_path')
+
+class ListingStatusSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Media
+        fields = ('id', 'status_string')
+
+class ListingTypeSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Media
+        fields = ('id', 'type_string')
 
 class ListingSchema(ma.SQLAlchemySchema):
     media = ma.List(ma.Nested(MediaSchema))
+    ltype = ma.Nested(ListingTypeSchema)
+    lstatus = ma.Nested(ListingStatusSchema)
     class Meta:
         model = Listing
-        fields = ('title', 'city', 'media')
+        fields = ('title', 'description', 'listing_price', 'media', 'ltype', 'lstatus')
 
 listing_schema = ListingSchema()
 listings_schema = ListingSchema(many=True)
@@ -28,8 +40,10 @@ listings_schema = ListingSchema(many=True)
 def get_listings_route():
     query_string = request.args.get('query','')
     query_filter = Listing.title.ilike(f'%{query_string}%')
-    house_type = request.args.get('house_type','')
-    house_type_filter = Listing.house_type.match(house_type)
-    filters = (or_(query_filter, house_type_filter))
+    listing_type = request.args.get('listing_type','Houses')
+    listing_status = request.args.get('listing_status','Verified')
+    listing_type_filter = Listing.listing_type.match(listing_type)
+    listing_status_filter = Listing.listing_type.match(listing_status)
+    filters = (or_(query_filter, listing_type_filter, listing_status_filter))
     filtered_result_set = Listing.query.filter().all()
     return listings_schema.jsonify(filtered_result_set)
