7347d2d2ad353a8c7eb2fba6479da3003f137816
Raviteja Guttula <thisisgvrt@gmail.com>
2020-07-27 13:43:23 -0700

    added listing detail

--- application/frontend/src/pages/HomePage.js
+++ application/frontend/src/pages/HomePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import listing from './listing';
+import { Link } from "react-router-dom";
 
 const HomePage = ({ listings }) => {
 
-    const getCard = ({ title, media, description, listing_price, num_baths, num_beds }) => {
+    const getCard = ({ title, media, description, listing_price, num_baths, num_beds }, idx) => {
         const media_img_source = media[0].media_path;
         return (
-            <div class="card col-sm-1 col-md-4 col-lg-3" style={{ "padding-top": "1.5%" }}>
-                <img src={(`/media?file_name=${media_img_source}`)} class="img-fluid card-img-top" style={{height: "15vw", "object-fit": "cover"}} alt="dummy" />
+            <div key={idx} class="card col-sm-1 col-md-4 col-lg-3" style={{ "padding-top": "1.5%" }}>
+                <Link to={`/listingDetail/${idx}`}>
+                    <img src={(`/media?file_name=${media_img_source}`)} class="img-fluid card-img-top" style={{height: "15vw", "object-fit": "cover"}} alt="dummy" />
+                </Link>
                 <div class="card-body">
                     <h4>{title}</h4>
                     <p className="text-truncate"> {description} </p>
@@ -23,7 +25,7 @@ const HomePage = ({ listings }) => {
                 {listings.length} listings found.
             </div>
             <div className="container-fluid row d-flex flex-wrap justify-content-between align-items-center" style={{ "margin-top": "0.5%" }}>
-                {listings.map(listing => getCard(listing))}
+                {listings.map((listing, idx) => getCard(listing, idx))}
             </div>
         </div>
     )
