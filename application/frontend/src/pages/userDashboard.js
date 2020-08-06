172b95930d35f666ef1af0f0830b8e94996dcee2
swetha867 <60675967+swetha867@users.noreply.github.com>
2020-08-06 14:34:59 -0700

    Connected backend api's in userDashboard page .

--- application/frontend/src/pages/userDashboard.js
+++ application/frontend/src/pages/userDashboard.js
@@ -1,13 +1,71 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from 'axios';
+
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 
 const UserDashBoard = ({dispatch }) => {
+ 
+
+
+    const [myListings, setMyListings] = useState([]);
+    const [listingsLength, setListingsLength] = useState(0);
+    const [showMesageTable, setShowMesageTable] = useState(false);
+
+    const [myMessages, setMyMessages] = useState([]);
+
+    const fetchMessages = () => {
+      axios.get(`/api/messaging`) 
+      .then((res) => {
+        setMyMessages(res.data);
+        if(res.data.length>0){
+          setShowMesageTable(true);
+        }
+        })
+        .catch(e => "error loading the  listings for receiver" + e)
+    }
+    const fetchMyListings = () => {
+      axios.get(`/api/myListings`) 
+      .then((res) => {
+        setMyListings(res.data);
+        setListingsLength(res.data.length);
+        })
+        .catch(e => "error loading the messages for receiver" + e)
+    }
+
+    React.useEffect(async () => {
+      fetchMyListings();
+      fetchMessages();
+    }, []);
+  
     
     return(
         <div class="container py-5">
-             <div class="row"><br/><h5>My Listings</h5><br/></div>
+          <div class="row"><br/><h5>My Listings</h5><br/></div>
+    <div class="row"><br/><h5>Total Number of Listings:{listingsLength}</h5><br/></div>
+
+           <div className='row'>
+
+{myListings.map(listing => (
+
+  <div key={listing.title} className='col-lg-4 col-md-3 col-sm-6'>
+    <div className="card">               
+    <img className="card-img-top" src={require(`${listing.media}`)} alt="Listing image cap"/>
+    <div className="card-body">
+
+      <h5 className="card-title">{listing.title}</h5>
+      <p className="card-text">${listing.price}</p>
+        <span className='txt-link'>
+        <Link to={`/listingDetail/${listing.idx}`}>      
+           <button>Info</button>
+          </Link>
+        </span>
+  </div>
+</div></div>
+))} 
+</div>
+             {/* <div class="row"><br/><h5>My Listings</h5><br/></div>
         <div class="row">
         <div class="card-deck">
   <div class="card">
@@ -32,42 +90,41 @@ const UserDashBoard = ({dispatch }) => {
     </div>
   </div>
    </div>   
-</div>
+</div> */}
+
       
         <div class="row"><br/><h5>Recieved messages</h5><br/></div>
         <div class="row">
+        { showMesageTable && (
+
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
+              {/* <th scope="col">#</th> */}
+              <th scope="col">Date</th>
               <th scope="col">Sender</th>
               <th scope="col">House title</th>
               <th scope="col">Message</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Apartments with balcony and blue borders</td>
-              <td>Please send me the  floorplan of the house to me this-is-totally-a-valid-email@mail.sfsu.edu</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Apartments with balcony and blue borders</td>
-              <td>I would like to get a tour of this house, can you please arrange for an in-person tour ?</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>Apartments with balcony and blue borders</td>
-              <td>I would like to get a tour of this house, can you please reach out to me at +1-123-456-6789</td>
-            </tr>
-          </tbody>
-        </table>
+            {myMessages.map(message => (
+                <tr>
+               <td>{message.timestamp}</td>
+               <td>{message.sender_id}</td>
+
+               <td>{message.listing_title}</td>
+
+                <td>{message.description}</td>
+                </tr>
+                
+                ))}
+                </tbody>
+          
+        </table>  )  }
         </div>
-        </div>
+         </div>       
+
     )
 }
 const mapStateToProps = (state) => {
